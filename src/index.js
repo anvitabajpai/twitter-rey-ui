b6a6c9ec2ac16d621cf8cf0201c96b4154268ecc
Anvita Bajpai <anvitabajpai@wa-abajpai-mbp.local>
2019-06-18 22:52:16 -0700

    Some logic for customized Cognito Greetings

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import Amplify from "aws-amplify";
+import Amplify, {Auth} from "aws-amplify";
 import { Provider } from 'react-redux';
 const rootEl = document.getElementById('app');
 import Routes from './Routes';
 import {store} from './containers/SearchSPA/store';
-import { withAuthenticator } from "aws-amplify-react";
 import awsMobile from './aws-exports';
+import { withAuthenticator,  Authenticator, Greetings,  SignIn, SignUp, ConfirmSignIn, VerifyContact, ConfirmSignUp, ForgotPassword, RequireNewPassword, Loading } from "aws-amplify-react";
 
 Amplify.configure(awsMobile);
+var userName = '';
+Auth.currentAuthenticatedUser()
+    .then(
+        (user) => {
+            if (user.name != undefined) {
+                userName = user.name;
+                console.log(userName);
+            } else if (user != undefined && user.attributes != undefined && user.attributes.email != undefined) {
+                userName = user.attributes.email;
+                console.log(user.attributes.email);
+            }
+            console.log(user);
+        }
+     ).catch(
+        err => console.log(err)
+     )
+
+
+console.log(Auth.federatedSignIn.name);
+
 
 class App extends React.Component {
     constructor(props, context) {
@@ -16,15 +36,23 @@ class App extends React.Component {
     }
 
     render() {
-        return (
-            <Provider store={store}>
-                   <div>
-                      <Routes />
-                   </div>
-            </Provider>
-        );
+        if (this.props.authState == "signedIn") {
+            return (
+                <Provider store={store}>
+                    <div>
+                        <Greetings
+                            inGreeting={(username) => 'Hi ' + userName}
+                        />
+                        <Routes />
+                    </div>
+                </Provider>
+            );
+        } else {
+            return null;
+        }
     }
 }
+
 
 const federated = {
     google_client_id: '157100821509-0njnajfki2nl6noptpf06kd2apsr91kp.apps.googleusercontent.com', // Enter your google_client_id here
@@ -32,10 +60,14 @@ const federated = {
     amazon_client_id: '' // Enter your amazon_client_id here
 };
 
-const AppWithAuth = withAuthenticator(App, {includeGreetings: true});
+
+const AppWithAuth = withAuthenticator(
+    App,
+    false,
+);
 
 ReactDOM.render(
-    <AppWithAuth federated={federated}/>,
+    <AppWithAuth federated={federated} />,
     rootEl,
 );
 
